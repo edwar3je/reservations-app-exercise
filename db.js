191f0977674c41728d9bae5282b46861521e5cf3
edwar3je <jameserikedwards@gmail.com>
2023-08-25 03:15:27 -0400

    first version

---
+++ db.js
@@ -0,0 +1,12 @@
+/** Database for lunchly */
+
+const pg = require("pg");
+
+const db = new pg.Client({
+    host: "/var/run/postgresql/",
+    database: "lunchly"
+});
+
+db.connect();
+
+module.exports = db;
